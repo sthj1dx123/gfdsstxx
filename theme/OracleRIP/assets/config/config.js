75a033c0bca4adf68c14d63992779328a97d6920
sthj1dx123 <143556405+sthj1dx123@users.noreply.github.com>
2023-08-30 23:21:44 +0800

    Add files via upload

---
+++ theme/OracleRIP/assets/config/config.js
@@ -0,0 +1,22 @@
+//exp: window.settings.planCheckout:{content:'欢度xxx 通用优惠码', coupon:'XJSW2022'},
+window.settings.planCheckout = {content:'新用户可用优惠码:', coupon:'NEWUR10%OFF'};
+//Don`t fucking change this window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
+window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
+window.settings.footerEmail = 'Fast & Stable.';
+// /plan 套餐展示页下方 FAQ
+window.settings.buyPlan_faq=[
+        {question:'我遇到问题了该怎么做?', answer:'若<使用教程>无法帮助到您,请点击网页右下角联系客服~'},
+        {question:'我还没有下载小火箭怎么办?', answer:'您可以在使用教程中找到共享的下载ID.'},
+        {question:'经典套餐与不限时流量包的区别?', answer:'经典套餐有到期时间(按时间付费)，流量包无到期时间；流量用完即止(按量付费).'},
+        {question:'可以观看奈飞/D+等流媒体吗?', answer:'可以的,我们使用行业领先的DnsMap流媒体解锁技术.'},
+        {question:'节点的延迟越低代表网速越快吗?', answer:'延迟(ms)只能作为一个参考数据,与网速并无直接联系,若您感觉当前节点有卡顿那么大概率是使用该节点的用户数量较多,请切换节点使用~'},
+        {question:'我不想付费,如何免费使用?', answer:'您可点击左上角菜单栏<邀请返利>,生成邀请码后将邀请链接发送给您的朋友,在他们购买后您将获得其支付金额的20%佣金(佣金可提现!首次邀请佣金翻倍~)'}
+        ];
+// /traffic 流量明细页上方 手风琴FQA
+//window.settings.userTraffic_faq = [
+//        {question:'dd', answer:'22'},
+//        {question:'dd', answer:'22'},
+//        {question:'dd', answer:'22'},
+//        {question:'dd', answer:'22'},
+//        {question:'dd', answer:'22'},
+//    ];
